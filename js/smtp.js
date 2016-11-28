dd6f77b444f5c39acd22d90e327264ef180f023e
Melissa Lobo <mellobomel@gmail.com>
2016-11-28 02:28:58 -0200

    ajustando script de email

--- js/smtp.js
+++ js/smtp.js
@@ -2,7 +2,7 @@
   send: function(t, e, o, n, d, r, c) {
     var a = Math.floor(1e6 * Math.random() + 1),
       m = "http://smtpjs.com/smtp.aspx?";
-    m += "From=contatogogetterz@gmail.com" + t, m += "&to=contatogogetterz@gmail.com" + e, m += "&Subject=" + encodeURIComponent(o), m += "&Body=message" + encodeURIComponent(n), void 0 == d.token ? (m += "&Host=smtp.gmail.com" + d, m += "&Username=contatogogetterz@gmail.com" + r, m += "&Password=d115b935-938d-47da-9c08-e9282bab1bdd" + c, m += "&Action=Send") : (m += "&SecureToken=d115b935-938d-47da-9c08-e9282bab1bdd" + d.token, m += "&Action=SendFromStored"), m += "&cachebuster=" + a, Email.addScript(m)
+    m += "From=contatogogetterz@gmail.com" + t, m += "&to=contatogogetterz@gmail.com" + e, m += "&Subject=name" + encodeURIComponent(o), m += "&Body=message" + encodeURIComponent(n), void 0 == d.token ? (m += "&Host=smtp.gmail.com" + d, m += "&Username=contatogogetterz@gmail.com" + r, m += "&Password=ede1000d-075b-40bc-a24d-ac2030d7e1b6" + c, m += "&Action=Send") : (m += "&SecureToken=ede1000d-075b-40bc-a24d-ac2030d7e1b6" + d.token, m += "&Action=SendFromStored"), m += "&cachebuster=" + a, Email.addScript(m)
   },
   addScript: function(t) {
     var e = document.createElement("link");
